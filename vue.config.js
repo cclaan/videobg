a7b690e61763d4a5f208fa3ac8128fce4f79174b
cc laan <cc@laan.com>
2021-09-15 12:27:54 -0500

    source map remove

--- vue.config.js
+++ vue.config.js
@@ -11,19 +11,30 @@ const express = require('express');
 
 
 module.exports = {
+  
+  productionSourceMap: false,
+
   transpileDependencies: [
     'vuetify'
   ],
+  
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
     : '/',
   configureWebpack: {
+    
     devServer: {
+
       before: app => {
         app.use('/node_modules/', express.static(path.resolve(__dirname, 'node_modules')))
       },
+
       headers: { "Cross-Origin-Embedder-Policy" : "require-corp",
                   "Cross-Origin-Opener-Policy" : "same-origin" }
     }
+
+
+
+
   }
 };
